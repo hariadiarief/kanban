a18ffc9ceb5852c16aee57eaf3f43c8419e31ba7
hariadiarief <adi.arief77@gmail.com>
2025-01-22 19:18:56 +0700

    fix error

--- src/features/authentication/login.tsx
+++ src/features/authentication/login.tsx
@@ -2,12 +2,9 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardFooter } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { useAuth } from '@/context/auth/authContext'
 import { useState } from 'react'
 
 export default function Login() {
-  const { dispatch } = useAuth()
-
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
@@ -36,7 +33,7 @@ export default function Login() {
   // })
 
   const handleSubmit = async (e: React.FormEvent) => {
-    // e.preventDefault()
+    e.preventDefault()
     // loginMutation({
     //   identifier: email,
     //   password
